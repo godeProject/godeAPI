728914cc88c15eddd990db92584ee4b3f6fbdab7
Jakkaphat Chalermpanaphan <gormairusina.wrp@gmail.com>
2021-09-29 11:08:50 +0700

    add main files

---
+++ api.py
@@ -0,0 +1,48 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def spl(x):
+    return [char for char in x]
+
+def getans(x):
+    tha = ['ๅ','/','-','ภ','ถ','ุ','ึ','ค','ต','จ','ข','ช','+','๑','๒','๓','๔','ู','฿','๕','๖','๗','๘','๙','ๆ','ไ','ำ','พ','ะ','ั','ี','ร','น','ย','บ','ล','๐','"','ฎ','ฑ','ธ','ํ','ี','ณ','ฯ','ญ','ฐ',',','ฟ','ห','ก','ด','เ','้','่','า','ส','ว','ง','ฤ','ฆ','ฏ','โ','ฌ','็','๋','ษ','ศ','ซ','.','ผ','ป','แ','อ','ิ','ื','ท','ม','ใ','ฝ','(',')','ฉ','ฮ','ฺ','์','?','ฒ','ฬ','ฦ']
+    eng = ['1','2','3','4','5','6','7','8','9','0','-','=','!','@','#','$','%','^','&','*','(',')','_','+','q','w','e','r','t','y','u','i','o','p','[',']','Q','W','E','R','T','Y','U','I','O','P','{','}','a','s','d','f','g','h','j','k','l',';',"'",'A','S','D','F','G','H','J','K','L',':','"','z','x','c','v','b','n','m',",",'.','/','Z','X','C','V','B','N','M','<','>','?']
+    phrase = spl(x)
+    phraselength = len(phrase)
+    ans = []
+    i = 0
+    while i<phraselength:
+        if phrase[i] in tha:
+            ans.append(eng[tha.index(phrase[i])])
+        elif phrase[i] in eng:
+            ans.append(tha[eng.index(phrase[i])])
+        elif phrase[i]==' ':
+            ans.append(' ')
+        else:
+            ans.append(phrase[i])
+        i+=1
+    answer = ''.join(ans)
+    return answer
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+@app.route('/api/v1/getans', methods=['GET'])
+def api_id():
+    if 'phrase' in request.args:
+        phrase = str(request.args['phrase'])
+        results = getans(phrase)
+    else:
+        return "Error: No phrase provided."
+    return results
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)
