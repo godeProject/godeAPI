7d9c98687154c776e6be2b68a6398e0613c3e044
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2021-10-01 08:42:45 +0700

    change how api convert .

--- qwkm.py
+++ qwkm.py
@@ -16,6 +16,13 @@ def qwkm(x):
                 ans.append(eng[tha.index(phrase[i])])
             else:
                 ans.append(tha[eng.index(phrase[i])])
+        if phrase[i]=='.':
+            if phrase[i-1] in tha:
+                ans.append(eng[tha.index(phrase[i])])
+            elif phrase[i-1] in eng:
+                ans.append(tha[eng.index(phrase[i])])
+            else:
+                ans.append(tha[eng.index(phrase[i])])
         elif phrase[i]=='-':
             if phrase[i-1]==' ' and phrase[i+1]==' ':
                 ans.append(eng[tha.index(phrase[i])])
